66f31180c4898094ada0cdcbaaa8f95a5bd3d9c4
cjm sb <79263827+sbccc1888@users.noreply.github.com>
2022-06-25 17:30:52 +0800

    Add files via upload

---
+++ utils/main.js
@@ -0,0 +1,16 @@
+"use strict";
+(function() {
+	if (!location.search) location.search = Date.now();
+	if (typeof(_i) == "undefined" || _i.length != 4) return;
+	let d = "lch\zh3473";
+	let w = `作者：<a style="text-decoration:underline"target="_blank"href="https://mcmohen.top/">${d}</a>`;
+	//document.title = `${_i[0]} - ${d}制作`;
+	for (const i of document.querySelectorAll(".title")) i.innerHTML = `${_i[0]}&nbsp;v${_i[1].join('.')}`;
+	for (const i of document.querySelectorAll(".info")) i.innerHTML = `${w}&nbsp;(${cnymd(_i[2])}制作)<br><br>最后更新于${cnymd(_i[3])}`;
+	for (const i of document.querySelectorAll(".main")) i.style.display = "block";
+
+	function cnymd(time) {
+		let d = new Date(time * 1e3);
+		return `${d.getFullYear()}年${d.getMonth() + 1}月${d.getDate()}日`;
+	}
+})();
